59d80ec0dc0b7bb6daf7730377770dff8033dd2b
Gian Paul Flores <iosdev@IOSs-Mac-Pro.local>
2018-01-06 18:14:46 -0800

    Added tab bar menu for ERAS page

---
+++ iComPAsS/ExercisesItemBarViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ExercisesItemBarViewController.swift
+//  iComPAsS
+//
+//  Created by Gian Paul Flores on 06/01/2018.
+//  Copyright © 2018 University of Santo Tomas. All rights reserved.
+//
+
+import UIKit
+
+class ExercisesItemBarViewController: UIViewController {
+
+    @IBOutlet weak var hamburgerMenu : UIBarButtonItem!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        
+        if self.revealViewController() != nil
+        {
+            hamburgerMenu.target = self.revealViewController()
+            hamburgerMenu.action = #selector(SWRevealViewController.revealToggle(_:))
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
+}
